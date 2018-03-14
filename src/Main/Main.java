5e4b8469054cf1c66f6a78352bfca0f55a700e55
Unknown <lombardoflorian@gmail.com>
2018-03-14 17:33:12 +0100

    parser lire, début calcul cout itineraire

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,18 @@
 package Main;
 
+import ComplementRessource.Parser;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Parser parse = new Parser();
+        parse.lire();
     }
 }
