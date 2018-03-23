bd6ff7ced64deb498b63fb66f52472ec9a4074e4
Unknown <natjolivet@hotmail.fr>
2018-03-23 21:49:26 +0100

    debut classe Solution

--- src/factory/Solution.java
+++ src/factory/Solution.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 
 public class Solution {
 
-    private Itineraire iti;
     private int id;
     private ArrayList<Itineraire> sol;
 
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<Itineraire> getSol() {
+        return sol;
+    }
 
     public Solution(int id){
         this.id = id;
     }
 
+    public ArrayList<Solution> getVoisinage(){
+        return null;
+    }
 
 
 }
