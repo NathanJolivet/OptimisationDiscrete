9b001462f6e7e47d47e2e74e8e7b316f66ee50b0
Unknown <natjolivet@hotmail.fr>
2018-04-29 19:24:38 +0200

    recup

--- src/Algorithmes/Recuit.java
+++ src/Algorithmes/Recuit.java
@@ -6,6 +6,8 @@ public class Recuit implements AlgoStrategy {
 
     @Override
     public Solution recherche(Graph graph) {
+        Solution x0 = graph.getSolutionInitiale();
+        
         return null;
     }
 }
